e317dc2431136db3cbceb2242678d2d4b1886e01
Emrio <emrioisme@gmail.com>
2018-06-28 18:59:05 +0200

    added basics
    bot ready
    bot error/warn
    login

--- bot.js
+++ bot.js
@@ -1,2 +1,29 @@
-const Discord = require('discord.js');
-const emrioutils = require('emrioutils');
+const Discord = require('discord.js')
+const emrioutils = require('emrioutils')
+const fs = require('fs')
+const login = require("./assets/login")
+
+// The Bot Object
+const Bot = new Discord.Client()
+
+// Logs the bot in discord
+login(Bot)
+
+
+
+
+
+/* BOT READY EVENT */
+Bot.on("ready", () => {
+  console.log("Connecté!")
+})
+
+/* ERROR EVENT */
+Bot.on("error", (err) => {
+  console.error(err);
+})
+
+/* WARNING EVENT */
+Bot.on("warn", (warn) => {
+  console.warn(warn);
+})
