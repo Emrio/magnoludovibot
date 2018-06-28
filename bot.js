1a061d75d7117ac2e9469ef1c8e93d1fc11c7716
Emrio <emrioisme@gmail.com>
2018-06-28 19:24:16 +0200

    on new member

--- bot.js
+++ bot.js
@@ -10,8 +10,23 @@ const Bot = new Discord.Client()
 login(Bot)
 
 
+/* SERVER NEW MEMBER EVENT */
+Bot.on("guildMemberAdd", (member) => {
 
+  console.log("[Server Management] New member joined")
 
+  var guild = member.guild
+  var invitedRole = guild.roles.find("name", "Invités")
+
+  member.addRole(invitedRole)
+    .then(console.log)
+    .catch(console.error)
+
+  var welcomeText = "Bienvenue sur le serveur Discord du Lycée Louis-Le-Grand de la part de l'équipe de l'administration.\nJe suis Louis XIV, le bot principal du serveur. Tu peux voir la liste des commandes disponibles avec le `!help`.\nJe te conseille de lire le règlement du serveur puis de te présenter dans le channel `#présentation`. Si tu es un élève du lycée, merci de signaler ta classe dans ta présentation. Sinon, merci d'indiquier les raisons pour lesquels tu nous rejoins !\n\Je te souhaite de passer un bon temps avec nous !"
+
+  member.user.send(welcomeText)
+
+})
 
 /* BOT READY EVENT */
 Bot.on("ready", () => {
