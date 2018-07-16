e0e90d876a9c6d946ee9f137694cab0543322818
Emrio <emrioisme@gmail.com>
2018-07-16 11:46:27 +0200

    now using absolute paths
    avoid problem with launching in another directory

--- assets/login.js
+++ assets/login.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js')
 const emrioutils = require('emrioutils')
+const path = require('path')
 const fs = require('fs')
 
 module.exports = (Bot) => {
 
-  fs.readFile("./assets/config.json", (err, data) => {
-
+  fs.readFile(path.join(__dirname, 'config.json'), (err, data) => {
     var config = JSON.parse(data)
     Bot.login(config.token)
 
