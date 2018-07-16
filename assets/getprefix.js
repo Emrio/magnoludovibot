64248f5f4629287423e6c9c78384bff50d5f2040
Emrio <emrioisme@gmail.com>
2018-07-16 19:11:00 +0200

    also for config

--- assets/getprefix.js
+++ assets/getprefix.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js')
 const emrioutils = require('emrioutils')
+const path = require('path')
 const fs = require('fs')
 
 module.exports = () => {
 
-  var config = JSON.parse(fs.readFileSync("./assets/config.json"))
+  var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'assets', 'config.json')))
   return config.prefix
 }
